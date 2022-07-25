bc02addc425bd3c4cb02359fb24201b253961ddb
Amalie Shi <hshi16@jhu.edu>
2022-07-25 08:25:06 -0400

    Add documentations of the thought process

--- ranklist.py
+++ ranklist.py
@@ -1,8 +1,7 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-
-def graph(df, column_name):
+def ranklist(df, column_name):
     """
         This function provides the barplot for the each series in a column and plot it in bar plot
 
@@ -22,28 +21,4 @@ def graph(df, column_name):
     plt.title(column_name, fontsize=30)
     plt.show()
 
-def top(df,column_name)->str:
-    """
-    :param df: the dataframe that got passed into the function
-    :param column_name: the variable that we are interested in ranking in descending order
-    :return: a string of the top five products in the rank list of the column category
-    """
-    df = df.sort_values([column_name], ascending=False).reset_index(drop=True)
-    string1 = 'Top: '.append(", ".join(str(i) for i in list(df['name'].head())))
-    return string1
-
-
-def bottom(df,column_name)->str:
-    """
-    :param df: the dataframe that got passed into the function
-    :param column_name: the variable that we are interested in ranking in descending order
-    :return: a string of the bottom five products in the rank list of the column category
-    """
-    df = df.sort_values([column_name], ascending=False).reset_index(drop=True)
-    string2 = ", ".join(str(i) for i in list(df['name'].tail()))
-    return string2
-
-
-print(graph.__doc__)
-print(top.__doc__)
-print(bottom.__doc__)
+print(ranklist.__doc__)
